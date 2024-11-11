0aa8c9c6a94fdd858fcfb87942d7ec146a92dea9
ishiyayuki <79000323+yuki-studyWEB@users.noreply.github.com>
2024-11-11 19:33:52 +0900

    デザイン修正_自己紹介

--- pages/index.tsx
+++ pages/index.tsx
@@ -65,7 +65,7 @@ export default function Home({ allPostsData }: Props) {
                     width={165}
                     alt="プロフィール画像" // alt属性を追加
                 />
-                    <ul>
+                    <ul className={utilStyles.ml7}>
                         <li>石谷　悠貴（いしや　ゆうき）</li>
                         <li>神奈川県川崎市在住　30代</li>
                         <li>2019年からWeb業界に参入　Webコーダー歴約2年</li>
@@ -73,13 +73,13 @@ export default function Home({ allPostsData }: Props) {
                             2021年からフロントエンドエンジニアとしてWebサービス開発の業務に携わる
                         </li>
                     </ul>
-                    <div className={utilStyles.mt8}>
+                </div>
+                <div className={utilStyles.myProfile}>
                     はじめまして、石谷悠貴と申します。ユーザーエクスペリエンスを重視し、視覚的なデザインと機能性を両立したインターフェースを作成することを心がけてます。<br/>
-                    HTML/CSS: レスポンシブデザインを考慮したCSSの知識があります。<br/>
-                    JavaScript: JavaScriptから、Vue.js(2系,3系)を使った開発経験があります。<br/>また業務での経験はありませんが、Reactは公式ドキュメントやオンラインコースを通して学習を進めています。<br/>
-                    UI/UX: ユーザー視点でのデザインに配慮し、直感的なインターフェースの作成を心がけています。<br/>
-                    バックエンド連携: REST APIを通じたバックエンドとの連携をスムーズに行い、データ処理や非同期処理にも対応可能です。
-                    </div>
+                    <strong>HTML/CSS:</strong> レスポンシブデザインを考慮したCSSの知識があります。<br/>
+                    <strong>JavaScript:</strong> Vue.js(2系,3系)を使った開発経験があります。<br/>また業務での経験はありませんが、Reactは公式ドキュメントやオンラインコースを通して学習を進めています。<br/>
+                    <strong>UI/UX:</strong> ユーザー視点でのデザインに配慮し、直感的なインターフェースの作成を心がけています。<br/>
+                    <strong>バックエンド連携:</strong> REST APIを通じたバックエンドとの連携をスムーズに行い、データ処理や非同期処理にも対応可能です。
                 </div>
                 <ul className={utilStyles.jumpButtonList}>
                     <li className={utilStyles.jumpButtonItem}>
