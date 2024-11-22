f4dc781f0b51e3028a7b2888ab5cc91c1be1d0ac
ishiyayuki <79000323+yuki-studyWEB@users.noreply.github.com>
2024-11-22 17:27:45 +0900

    chage profileImg

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,7 +59,7 @@ export default function Home({ allPostsData }: Props) {
                 <div className={utilStyles.aboutMe}>
                 <Image
                     priority
-                    src="/images/profile.jpg"
+                    src="/images/profile_2.JPG"
                     className={utilStyles.borderCircle}
                     height={155}
                     width={165}
